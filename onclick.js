300b0fbf788339c0f373d4c684bbcd3abf4ac2a4
Gigatrone <gigicorte99@gmail.com>
2021-04-27 17:08:01 +0200

    sistemazione marche

---
+++ onclick.js
@@ -0,0 +1,85 @@
+
+function onclicker(classe){
+  if(classe=='rebecca'){
+  const element = document.querySelector('.rebeccapres');
+  element.classList.remove('visible');
+  
+}
+else {
+  const element = document.querySelector('.rebeccapres');
+  element.classList.add('visible');
+}
+
+
+if(classe=='magnagrecia'){
+  const element = document.querySelector('.magnagreciapres');
+  element.classList.remove('visible');
+
+}
+else {
+  const element = document.querySelector('.magnagreciapres');
+  element.classList.add('visible');
+}
+
+if(classe=='marea'){
+  const element = document.querySelector('.mareapres');
+  element.classList.remove('visible');
+}
+
+else {
+  const element = document.querySelector('.mareapres');
+  element.classList.add('visible');
+}
+
+if(classe=='bysimon'){
+  const element = document.querySelector('.bysimonpres');
+  element.classList.remove('visible');
+}
+else{ 
+const element = document.querySelector('.bysimonpres');
+  element.classList.add('visible');
+}
+
+if(classe=='maraco'){
+  const element = document.querySelector('.maracopres');
+  element.classList.remove('visible');
+}
+else{ 
+const element = document.querySelector('.maracopres');
+  element.classList.add('visible');
+}
+
+if(classe=='timberland')
+{
+  const element = document.querySelector('.timberlandpres');
+  element.classList.remove('visible');
+}
+else{ 
+const element = document.querySelector('.timberlandpres');
+  element.classList.add('visible');
+}
+
+if(classe=='antica_murrina'){
+  const element = document.querySelector('.antica_murrinapres');
+  element.classList.remove('visible');
+}
+else{ 
+const element = document.querySelector('.antica_murrinapres');
+  element.classList.add('visible');
+}
+
+if(classe=='anellissimi'){
+  const element = document.querySelector('.anellissimipres');
+  element.classList.remove('visible');
+}
+else{ 
+const element = document.querySelector('.anellissimipres');
+  element.classList.add('visible');
+}
+
+  
+
+//magnagrecia marea bysimon maraco timberland antica_murrina anellissimi
+/*      //magnagrecia marea bysimon maraco timberland antica_murrina anellissimi
+        .magnagreciapres .mareapres .bysimonpres .maracopres .timberlandpres .antica_murrinapres .anellissimipres*/ 
+}
